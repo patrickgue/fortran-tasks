7fc4c2b67ab8fa7cd3aa12d13968535d035dca77
Patrick Guenthard <me@patrickguenthard.ch>
2023-07-23 11:34:30 +0200

    Delete tasks

--- tasks.f
+++ tasks.f
@@ -2,7 +2,7 @@
       USE M_TASK
       IMPLICIT NONE
 
-      INTEGER :: COUNT, INP, I
+      INTEGER :: COUNT, INP, I, J
       LOGICAL :: LOOP
       TYPE(T_TASK), ALLOCATABLE :: TASK(:)
       TYPE(T_TASK), ALLOCATABLE :: TEMP(:)
@@ -22,7 +22,7 @@
          WRITE (*,'(A)', ADVANCE="NO") '> '
          READ (*,*) INP
 
-         IF (INP .eq. 1) THEN
+         IF (INP .EQ. 1) THEN
             CALL LIST_TASKS(TASK, COUNT)
          ELSE IF (INP .eq. 2) THEN
 C     ;;;;; REALLOCATE TASK
@@ -30,17 +30,41 @@ C     ;;;;; REALLOCATE TASK
             DO i = 1, COUNT
                TEMP(i) = TASK(i)
             END DO
-            DEALLOCATE(task)
+            DEALLOCATE(TASK)
             ALLOCATE(TASK(COUNT + 1))
             do i = 1, COUNT
-               TASK(i) = TEMP(i)
+               TASK(I) = TEMP(I)
             END do
             DEALLOCATE(TEMP)
             TASK(COUNT + 1) = NEW_TASK(COUNT)
             COUNT = COUNT + 1
             CALL TASKS_STORE(TASK, COUNT)
          ELSE IF (INP .eq. 3) THEN
-            WRITE (*,'(A)') '[Not yet implemented]'
+ 100        FORMAT(A)
+            WRITE (*,100) 'ID of Task to be deleted (-1 to cancel)'
+            WRITE (*,100,ADVANCE="NO") '# '
+            READ (*,*) INP
+            IF (INP .GE. 1 .AND. INP .LE. COUNT) THEN
+               ALLOCATE(TEMP(COUNT))
+               DO I = 1, COUNT
+                  TEMP(I) = TASK(I)
+               END DO
+               DEALLOCATE(TASK)
+               ALLOCATE(TASK(COUNT - 1))
+               J = 0
+               DO I = 1, COUNT
+                  IF (.NOT. TEMP(I)%ID .EQ. INP) THEN
+                     TASK(J) = TEMP(I)
+                     J = J + 1
+                  END IF
+               END DO
+               DEALLOCATE(TEMP)
+               COUNT = COUNT - 1
+               CALL TASKS_STORE(TASK, COUNT)
+            ELSE
+ 101           FORMAT(AI2A3I3)
+               WRITE(*,101) 'Input is outside range', 0, '-', COUNT
+            END IF
          ELSE IF (INP .eq. 4) THEN
             LOOP = .FALSE.
          END IF
